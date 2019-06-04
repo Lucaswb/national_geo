112700ba9678df8d023411e8e244dc04ac13e8fd
Erik Irgens and Lucas Blasdel <lucas@>
2019-06-04 10:24:24 -0700

    got images to work

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#panda-showing1").toggle();
+    $("#panda-hidden1").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#panda-showing2").toggle();
+    $("#panda-hidden2").toggle();
+  });
+});
